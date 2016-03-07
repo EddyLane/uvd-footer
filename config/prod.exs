dea3e50c479aacf2a52949cecd1554b50f408583
Eddy Lane <naedin@gmail.com>
2016-03-07 12:38:36 +0000

    Disabled origin check on prod

--- config/prod.exs
+++ config/prod.exs
@@ -15,6 +15,7 @@ config :uvd_footer, UvdFooter.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "shrouded-cliffs-95819.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  check_origin: false,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
